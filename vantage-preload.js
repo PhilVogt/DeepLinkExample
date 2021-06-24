d574e2a3f948a5e97393800f9a3ccf8217aa4c74
PhilVogt <philip.vogt@openfin.co>
2021-06-24 19:34:47 +0100

    Revert last change, didnt work.

--- vantage-preload.js
+++ vantage-preload.js
@@ -1,5 +1,5 @@
 
-function initVantage() {
+function init() {
     const button = document.createElement("button");
     button.innerHTML = "Click me";
 
@@ -14,24 +14,6 @@ function initVantage() {
     document.body.appendChild(input);
 }
 
-const initSalesForce = () => {
-
-    fin.desktop.main(userAppConfigArgs => {
-        alert(JSON.stringify(userAppConfigArgs));
-    });
-    let app = fin.Application.getCurrentSync();
-    // If app is already running parameters are passed through the “run-requested” event
-    app.addListener("run-requested", function (event) {
-        if (event.userAppConfigArgs) {
-            alert(`Update ${JSON.stringify(event.userAppConfigArgs)}`);
-        }
-    });
-};
-
 window.addEventListener("DOMContentLoaded", evt => {
-    if (fin.me.identity.name === "VantageView") {
-        initVantage();
-    } else if (fin.me.identity.name === "SalesForceView") {
-        initSalesForce();
-    }
+    init();
 });
