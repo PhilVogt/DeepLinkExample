46543d14dc5930c44570a6a9b410152695b2639f
PhilVogt <philip.vogt@openfin.co>
2021-06-24 19:49:40 +0100

    Switch to using a container window for SF

--- salesforce-preload.js
+++ salesforce-preload.js
@@ -1,13 +1,20 @@
-const initOpenFinParamListener = () => {
 
+
+const initOpenFinParamListener = () => {
+    let currentClientId;
     fin.desktop.main(userAppConfigArgs => {
-        alert(JSON.stringify(userAppConfigArgs));
+        if (currentClientId !== userAppConfigArgs.clientid) {
+            currentClientId = userAppConfigArgs.clientid;
+        }
     });
     let app = fin.Application.getCurrentSync();
     // If app is already running parameters are passed through the “run-requested” event
     app.addListener("run-requested", function (event) {
         if (event.userAppConfigArgs) {
-            alert(`Update ${JSON.stringify(event.userAppConfigArgs)}`);
+            if (currentClientId !== event.userAppConfigArgs.clientid) {
+                currentClientId = event.userAppConfigArgs.clientid;
+                alert(`Update ${currentClientId}`);
+            }
         }
     });
 };
